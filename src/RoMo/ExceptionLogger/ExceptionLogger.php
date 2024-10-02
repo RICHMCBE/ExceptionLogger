c2a43a6f1222ed7032329a6e300ea58670f7f862
bluefirewolf534 <bluefirewolf534@gmail.com>
2024-10-02 20:01:01 +0900

    :sparkles: 로그 줄 바꿈 수정, CommandSender에게도 메시지 전송이 가능하도록 수정

--- src/RoMo/ExceptionLogger/ExceptionLogger.php
+++ src/RoMo/ExceptionLogger/ExceptionLogger.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace RoMo\ExceptionLogger;
 
-use pocketmine\player\Player;
+use pocketmine\command\CommandSender;
 use pocketmine\Server;
 
 class ExceptionLogger{
-    public static function handleException(\Throwable $e, ?Player $usingPlayer = null) : void{
+    public static function handleException(\Throwable $e, ?CommandSender $usingSender = null) : void{
         $server = Server::getInstance();
         $path = $server->getDataPath() . "exception_logs/";
         if(!is_dir($path)){
@@ -16,15 +16,15 @@ class ExceptionLogger{
         }
         $fileName = date("D_M_j-H.i.s-T_Y") . ".log";
         $path .= $fileName;
-        $content = "error message: " . $e->getMessage() . "\n--- trace ---" . $e->getTraceAsString();
+        $content = "error message: " . $e->getMessage() . "\n--- trace ---\n" . $e->getTraceAsString();
         if($e->getPrevious() !== null){
             $previous = $e->getPrevious();
-            $content .= "\n\nprevious message: " . $previous->getMessage() . "\n--- previous trace ---" . $previous->getTraceAsString();
+            $content .= "\n\nprevious message: " . $previous->getMessage() . "\n--- previous trace ---\n" . $previous->getTraceAsString();
         }
         $server->getLogger()->error("New exception log has been created. log name: {$fileName}");
         file_put_contents($path, $content);
 
-        $usingPlayer?->sendMessage("§l§6 • §r§7오류가 발생하였습니다. ({$fileName})");
-        $usingPlayer?->sendMessage("§l§6 • §r§7관리자에게 해당 메시지가 포함된 스크린샷과 함께 문의해주세요.");
+        $usingSender?->sendMessage("§l§6 • §r§7오류가 발생하였습니다. ({$fileName})");
+        $usingSender?->sendMessage("§l§6 • §r§7관리자에게 해당 메시지가 포함된 스크린샷과 함께 문의해주세요.");
     }
 }
