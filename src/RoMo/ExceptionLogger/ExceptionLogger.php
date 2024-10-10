fcbf95fa4f082f696487f8e980c03d8def8e630f
bluefirewolf534 <bluefirewolf534@gmail.com>
2024-10-11 00:16:16 +0900

    :zap: 오류 로그를 남길 때 오류가 발생했을 때를 대응

--- src/RoMo/ExceptionLogger/ExceptionLogger.php
+++ src/RoMo/ExceptionLogger/ExceptionLogger.php
@@ -9,22 +9,29 @@ use pocketmine\Server;
 
 class ExceptionLogger{
     public static function handleException(\Throwable $e, ?CommandSender $usingSender = null) : void{
-        $server = Server::getInstance();
-        $path = $server->getDataPath() . "exception_logs/";
-        if(!is_dir($path)){
-            mkdir($path);
-        }
-        $fileName = date("D_M_j-H.i.s-T_Y") . ".log";
-        $path .= $fileName;
-        $content = "error message: " . $e->getMessage() . "\n--- trace ---\n" . $e->getTraceAsString();
-        if($e->getPrevious() !== null){
-            $previous = $e->getPrevious();
-            $content .= "\n\nprevious message: " . $previous->getMessage() . "\n--- previous trace ---\n" . $previous->getTraceAsString();
-        }
-        $server->getLogger()->error("New exception log has been created. log name: {$fileName}");
-        file_put_contents($path, $content);
+        try{
+            $server = Server::getInstance();
+            $path = $server->getDataPath() . "exception_logs/";
+            if(!is_dir($path)){
+                mkdir($path);
+            }
+            $fileName = date("D_M_j-H.i.s-T_Y") . ".log";
+            $path .= $fileName;
+            $content = "error message: " . $e->getMessage() . "\n--- trace ---\n" . $e->getTraceAsString();
+            if($e->getPrevious() !== null){
+                $previous = $e->getPrevious();
+                $content .= "\n\nprevious message: " . $previous->getMessage() . "\n--- previous trace ---\n" . $previous->getTraceAsString();
+            }
+            $server->getLogger()->error("New exception log has been created. log name: {$fileName}");
+            file_put_contents($path, $content);
 
-        $usingSender?->sendMessage("§l§6 • §r§7오류가 발생하였습니다. ({$fileName})");
-        $usingSender?->sendMessage("§l§6 • §r§7관리자에게 해당 메시지가 포함된 스크린샷과 함께 문의해주세요.");
+            $usingSender?->sendMessage("§l§6 • §r§7오류가 발생하였습니다. ({$fileName})");
+            $usingSender?->sendMessage("§l§6 • §r§7관리자에게 해당 메시지가 포함된 스크린샷과 함께 문의해주세요.");
+        }catch(\Throwable $error){
+            Server::getInstance()->getLogger()->error("An error occurred while logging the exception. Error: {$error->getMessage()}");
+            Server::getInstance()->getLogger()->error($error->getTraceAsString());
+            Server::getInstance()->getLogger()->error("Original error: {$e->getMessage()}");
+            Server::getInstance()->getLogger()->error($e->getTraceAsString());
+        }
     }
 }
